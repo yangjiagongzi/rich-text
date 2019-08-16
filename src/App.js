3319eca0af689c4164ba6788f4ff7865621e7358
yangning <ning@edison.tech>
2019-08-16 13:46:51 +0800

    delnode fixbug

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 import { RichText } from './RichText';
 
+const AT_BEGIN_CHAR = '\u0005';
+const AT_END_CHAR = '\u0004';
+
+function MessageText(text) {
+  const regStr = `(${AT_BEGIN_CHAR}.*?${AT_END_CHAR})`;
+  const reg = new RegExp(regStr, 'g');
+  const newStr = text.replace(reg, `<span className="at">${'$1'}</span>`);
+  return (
+    <div
+      className="msg-text-with-at"
+      dangerouslySetInnerHTML={{ __html: newStr }}
+    />
+  );
+}
+
+function getTextFromHtml(str) {
+  let strFormat = '';
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = str;
+  const childs = tempDiv.childNodes;
+  childs.forEach(el => {
+    if (el.nodeType === 3) {
+      strFormat += el.nodeValue;
+    } else if (el.nodeType === 1) {
+      const jid = el.getAttribute('jid');
+      if (jid) {
+        strFormat += `${AT_BEGIN_CHAR}@${jid}${AT_END_CHAR}`;
+      }
+      if (el.nodeName === 'BR') {
+        strFormat += '\n';
+      }
+    }
+  });
+  return strFormat;
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -10,9 +46,10 @@ class App extends Component {
     };
   }
 
-  onchange(value) {
-    console.log('value change:', value);
-  }
+  onchange = value => {
+    const str = getTextFromHtml(value);
+    this.setState({ msgBody: str });
+  };
 
   render() {
     const keyMapping = [
@@ -55,6 +92,13 @@ class App extends Component {
           onChange={this.onchange}
           ref={el => (this._richText = el)}
         />
+        <button
+          onClick={() => {
+            this._richText.delNode(1);
+          }}
+        >
+          123
+        </button>
       </div>
     );
   }
